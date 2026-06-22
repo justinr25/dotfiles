5e336e9f2fc907d45e0212bc7f1f73943b98d3e9
Justin Rosales <justin06.rosales@gmail.com>
2026-06-22 13:16:42 -0500

    Flatten dotfiles directory structure for aerospace, ghostty, and sketchybar

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env sh
 
-# Create .config directory if it doesn't exist
+# Create config directories if they don't exist
 mkdir -p ~/.config
+mkdir -p ~/.config/aerospace
+mkdir -p ~/.config/ghostty
+mkdir -p ~/.config/sketchybar
 
 # Stow home directory configs
 stow -t ~ zsh
 
-# Stow .config directory configs
-stow -t ~/.config aerospace ghostty sketchybar starship
+# Stow app-specific config directories
+stow -t ~/.config/aerospace aerospace
+stow -t ~/.config/ghostty ghostty
+stow -t ~/.config/sketchybar sketchybar
+
+# Stow general config directory configs
+stow -t ~/.config starship
 
 echo "Dotfiles successfully stowed!"
