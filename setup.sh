9be2376acd8e4ccf34eac26a5b34c47271c32277
Justin Rosales <justin06.rosales@gmail.com>
2026-06-22 23:51:14 -0500

    Configure nvim settings and integrate tmux config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.config/aerospace
 mkdir -p ~/.config/ghostty
 mkdir -p ~/.config/sketchybar
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/tmux
 
 # Stow home directory configs
 stow -t ~ zsh
@@ -15,6 +16,7 @@ stow -t ~/.config/aerospace aerospace
 stow -t ~/.config/ghostty ghostty
 stow -t ~/.config/sketchybar sketchybar
 stow -t ~/.config/nvim nvim
+stow -t ~/.config/tmux tmux
 
 # Stow general config directory configs
 stow -t ~/.config starship
