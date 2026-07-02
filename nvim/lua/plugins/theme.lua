b5aed35dac155cca8d271e7343cad9cce9772a97
Justin Rosales <justin06.rosales@gmail.com>
2026-07-01 23:13:49 -0500

    feat(theme): support easy toggle between catppuccin and tokyonight

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,34 +1,62 @@
 -- ================================================================================================
--- TITLE : catpuccin
--- ABOUT : A popular, soothing colorscheme for Neovim
+-- TITLE : Theme Configuration
+-- ABOUT : Easily toggle between Catppuccin and Tokyonight
 -- LINKS :
---   > github : https://github.com/catppuccin/nvim
+--   > Catppuccin : https://github.com/catppuccin/nvim
+--   > Tokyonight : https://github.com/folke/tokyonight.nvim
 -- ================================================================================================
 
+-- Change this to "catppuccin" or "tokyonight" to switch themes!
+local active_theme = "catppuccin"
+-- local active_theme = "tokyonight"
+
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        transparent_background = true, -- Enable transparent background
-        show_end_of_buffer = false, -- Hide ~ at end of buffer
-        integrations = {
-          blink_cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
-          mini = {
-            enabled = true,
-            indentscope = true,
-          },
-        },
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  }
+	-- Catppuccin Configuration
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = active_theme ~= "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				transparent_background = true,
+				show_end_of_buffer = false,
+				integrations = {
+					blink_cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					mini = {
+						enabled = true,
+						indentscope = true,
+					},
+				},
+			})
+			if active_theme == "catppuccin" then
+				vim.cmd.colorscheme("catppuccin")
+			end
+		end,
+	},
+
+	-- Tokyonight Configuration
+	{
+		"folke/tokyonight.nvim",
+		lazy = active_theme ~= "tokyonight",
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				style = "moon", -- storm, moon, night, day
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			})
+			if active_theme == "tokyonight" then
+				vim.cmd.colorscheme("tokyonight")
+			end
+		end,
+	},
 }
